49332692f057b3018fb450bdf7363bfa37082732
Rob Jones <robert.buckingham.jones@gmail.com>
2014-04-12 22:28:35 +0100

    Add support for multiple feeds

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,56 @@
 'use strict';
 
 angular.module('djsreaderApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'http%3A%2F%2Fdailyjs.com%2Fatom.xml'%20and%20itemPath%3D'feed.entry'&format=json&diagnostics=true&callback=JSON_CALLBACK";
+  .controller('MainCtrl', function ($scope, $http, $timeout) {
+    $scope.refreshInterval = 60;
 
-    $http.jsonp(url).
-      success(function(data, status, headers, config) {
-        $scope.feed = {
-          title: 'DailyJS',
-          items: data.query.results.entry
-        };
-      }).
-      error(function(data, status, headers, config) {
-        console.error('Error fetchng feed:', data);
-      });
+    // var url = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'http%3A%2F%2Fdailyjs.com%2Fatom.xml'%20and%20itemPath%3D'feed.entry'&format=json&diagnostics=true&callback=JSON_CALLBACK";
+
+    // $http.jsonp(url).
+    //   success(function(data, status, headers, config) {
+    //     $scope.feed = {
+    //       title: 'DailyJS',
+    //       items: data.query.results.entry
+    //     };
+    //   }).
+    //   error(function(data, status, headers, config) {
+    //     console.error('Error fetchng feed:', data);
+    //   });
+
+    $scope.feeds = [{
+      url: 'http://dailyjs.com/atom.xml',
+      items: [ /* Blog posts go here */ ]
+    }]
+
+    $scope.addFeed = function(feed) {
+      $scope.feeds.push(feed);
+      $scope.fetchFeed(feed);
+      $scope.newFeed = {};
+    };
+
+    $scope.fetchFeed = function(feed) {
+      feed.items = [];
+
+      var apiURL = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20xml%20where%20url%3D'";
+      apiURL += encodeURIComponent(feed.url);
+      apiURL += "'%20and%20itemPath%3D'feed.entry'&format=json&diagnostics=true&callback=JSON_CALLBACK";
+
+      $http.jsonp(apiURL).
+        success(function(data, status, headers, config) {
+          if (data.query.results) {
+            feed.items = data.query.results.entry;
+          }
+        }).
+        error(function(data, status, headers, config) {
+          console.error('Error fetching feed:', data);
+        });
+    };
+
+    $scope.deleteFeed = function(feed) {
+      $scope.feeds.spice($scope.feeds.indexOf(feed), 1);
+    };
+
+    $timeout(function() { $scope.fetchFeed(feed); }, $scope.refreshInterval * 1000 );
+
+
   });
